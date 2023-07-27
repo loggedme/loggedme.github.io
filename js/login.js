76f586b26aee843fe268df003bdb71318ef2851a
kimyeonchul <yeonchul0807@nate.com>
2023-07-27 22:17:53 +0900

    Feat : Add ajax code in login.js(김연출)

--- js/login.js
+++ js/login.js
@@ -3,7 +3,32 @@ $(".login_login_btn").click(function () {
     $("#login_input_id").val().trim() &&
     $("#login_input_password").val().trim()
   ) {
-    console.log("success");
+    var formData = {
+      id: $("#login_input_id").val(),
+      password: $("#login_input_password").val(),
+    };
+    console.log(formData);
+    // $.ajax({
+    //   url: url,
+    //   type: "POST",
+    //   dataType: "json",
+    //   data: formData,
+    //   success: function (data) {
+    //     console.log(data);
+    //   },
+    //   error: function (request, status, error) {
+    //     console.log(
+    //       "code:" +
+    //         request.status +
+    //         "\n" +
+    //         "message:" +
+    //         request.responseText +
+    //         "\n" +
+    //         "error:" +
+    //         error
+    //     );
+    //   },
+    // });
   } else {
     alert("입력되지 않은 항목이 있습니다.");
   }
