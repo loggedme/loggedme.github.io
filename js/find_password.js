f7b29b2f7d7e7bff61c03e64bbd59ff027e440f5
kimyeonchul <yeonchul0807@nate.com>
2023-07-27 22:15:13 +0900

    Feat : Add find_password template(css,js,html) (김연출)

---
+++ js/find_password.js
@@ -0,0 +1,31 @@
+$(".find_password_main_btn").click(function () {
+  if ($(".find_password_main_input").val().trim()) {
+    var formData = {
+      email: $(".find_password_main_input").val(),
+    };
+    console.log(formData);
+    // $.ajax({
+    //   url: url,
+    //   type: "GET",
+    //   dataType: "json",
+    //   data: formData,
+    //   success: function (data) {
+    //     console.log(data);
+    //   },
+    //   error: function (request, status, error) {
+    //     console.log(
+    //       "code:" +
+    //         request.status +
+    //         "\n" +
+    //         "message:" +
+    //         request.responseText +
+    //         "\n" +
+    //         "error:" +
+    //         error
+    //     );
+    //   },
+    // });
+  } else {
+    alert("입력되지 않은 항목이 있습니다.");
+  }
+});
