e5527acf903cd1031816854568b295bf84e36317
kimyeonchul <yeonchul0807@nate.com>
2023-07-28 01:34:27 +0900

    Feat: Init making_badge template(css,html,js)(김연출)

---
+++ js/making_badge.js
@@ -0,0 +1,11 @@
+// function readUrl(event) {
+//   const file = event.target.files[0];
+
+//   const reader = new FileReader();
+
+//   reader.onload = function (event) {
+//     $("#making_badge_attach_image").src = event.target.result;
+//   };
+
+//   reader.readAsDataURL(file);
+// }
