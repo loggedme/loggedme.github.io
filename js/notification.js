ebe53978aaef835df124f686b8b6a106205dbfcb
kimyeonchul <yeonchul0807@nate.com>
2023-07-30 18:18:01 +0900

    Feat: Add using mockData function in notification(김연출)

---
+++ js/notification.js
@@ -0,0 +1,29 @@
+$.getJSON("../mock/notificationData.json", function (data) {
+  $.each(data, function (index, item) {
+    var itemElement = $("<a>").prop({
+      class: "item_element",
+      href: "#",
+    });
+    var imgElement = $("<img>").attr({
+      src: item.imageSrc,
+      alt: item.name,
+      class: "item_image",
+    });
+    var elementWrap = $("<div>").prop({
+      class: "content_wrap",
+    });
+    var contentElement = $("<div>").prop({
+      class: "item_content",
+      textContent: item.docuement,
+    });
+
+    var createdElement = $("<div>").prop({
+      class: "item_created_at",
+      textContent: item.created_at,
+    });
+    elementWrap.append(contentElement).append(createdElement);
+    itemElement.append(imgElement);
+    itemElement.append(elementWrap);
+    $(".notification_main_section").append(itemElement);
+  });
+});
