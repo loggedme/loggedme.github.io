29a7fc6589098dbaac2f26f321bea5f2c841c1ec
kimyeonchul <yeonchul0807@nate.com>
2023-07-30 20:36:43 +0900

    Feat: Add signup template(css,js,html)(김연출)

--- js/signup.js
+++ js/signup.js
@@ -1,16 +1,133 @@
-// 인증 button click event
-$(".signup_email_certification_button").click(function () {
-  if ($("#signup_input_email").val().trim()) {
-    console.log($("#signup_input_email").val().trim());
+// static variable
+var email_request = false; //인증보냈는지 여부
+var email_responese = false; //인증받았는지 여부
+var radio_bool = false; // 라디오박스 체크 여부
+var regEmail =
+  /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i; //이메일 정규식
+
+// email 관련 function
+function check_email() {
+  console.log(0);
+  var email = $("#signup_input_email");
+  var email_err_msg = $("#signup_input_err_msg_email");
+  if (!email.val().trim()) {
+    console.log(1);
+    email.focus();
+    return false;
+  } else if (regEmail.test(email.val().trim())) {
+    console.log(2);
+    email_err_msg.hide();
+    email.css("margin-bottom", "0.81rem");
+    return true;
   } else {
-    alert("이메일을 입력해주세요.");
+    console.log(3);
+    email_err_msg.show();
+    email.css("margin-bottom", "0");
+    email.focus();
+    return false;
+  }
+}
+
+// 인증번호 전송 button click event
+$(".signup_email_certification_button").click(function () {
+  email_request = false;
+  var email_certification_num = $("#signup_input_email_num");
+  if (check_email()) {
+    //ajax 코드
+    email_request = true;
+    email_certification_num.attr("disabled", false);
+    email_certification_num.focus();
+    return true;
+  } else {
+    email_request = false;
+    email_certification_num.attr("disabled", true);
+    return false;
   }
 });
+
+// 인증번호 확인 event function
+$(".signup_email_num_button").click(function () {
+  if (!email_request) {
+    $("#signup_input_email").focus();
+  } else if (!$("#signup_input_email_num").val().trim()) {
+    $("#signup_input_email_num").focus();
+  } else {
+    check_email_num();
+  }
+});
+
+function check_email_num() {
+  //ajax code
+  var certification_num = 1234; //test num
+  if ($("#signup_input_email_num").val().trim() == certification_num) {
+    alert("인증 성공");
+    $("#signup_input_email_num").attr("disabled", true);
+    $("#signup_input_name").focus();
+    email_responese = true;
+    return true;
+  } else {
+    alert("인증 실패");
+    $("#signup_input_email_num").focus();
+    email_responese = false;
+    return false;
+  }
+}
+
+function check_name() {
+  return $("#signup_input_name").val().trim() ? true : false;
+}
+
+function check_password() {
+  return $("#signup_input_password").val().trim() ? true : false;
+}
+
+function check_re_password() {
+  return $("#signup_input_re_password").val().trim() ? true : false;
+}
+
+//password 일치하는지 검사
+function password_matching() {
+  var pwd = $("#signup_input_password").val();
+  var re_pwd = $("#signup_input_re_password").val();
+  if (pwd === re_pwd) {
+    $("#signup_input_err_msg_re_password").hide();
+    $("#signup_input_re_password").css("margin-bottom", "0.81rem");
+    return true;
+  } else {
+    $("#signup_input_err_msg_re_password").show();
+    $("#signup_input_re_password").css("margin-bottom", "0");
+    return false;
+  }
+}
+
+//실시간 email input change event
+$("#signup_input_email").on(
+  "propertychange change keyup paste input",
+  function () {
+    check_email();
+  }
+);
+
+//re-password 실시간 change event
+$("#signup_input_re_password").on(
+  "propertychange change keyup paste input",
+  function () {
+    password_matching();
+  }
+);
+
+//re-password 실시간 change event
+$("#signup_input_password").on(
+  "propertychange change keyup paste input",
+  function () {
+    password_matching();
+  }
+);
 
 //radio button click event
 $("input:radio[name=radiobutton1]").click(function () {
   if ($("input:radio[name=radiobutton1]:checked").val() === "기업") {
-    $("#radiobutton1_label2").css("background-color", "#056CF2");
+    $("#radiobutton1_label2").css("background-color", "#6970f2");
     $("#radiobutton1_label2").css("color", "white");
     $("#radiobutton1_label1").css(
       "background-color",
@@ -18,7 +135,7 @@ $("input:radio[name=radiobutton1]").click(function () {
     );
     $("#radiobutton1_label1").css("color", "black");
   } else {
-    $("#radiobutton1_label1").css("background-color", "#056CF2");
+    $("#radiobutton1_label1").css("background-color", "#6970f2");
     $("#radiobutton1_label1").css("color", "white");
     $("#radiobutton1_label2").css(
       "background-color",
@@ -26,24 +143,22 @@ $("input:radio[name=radiobutton1]").click(function () {
     );
     $("#radiobutton1_label2").css("color", "black");
   }
+  radio_bool = true;
 });
 
 // ok button click event
 $(".signup_signup_btn").click(function () {
-  if (
-    !$("#signup_input_email_num").val().trim() &&
-    !$("#signup_input_name").val().trim() &&
-    !$("#signup_input_password").val().trim() &&
-    !$("#signup_input_re_password").val().trim()
-  ) {
-    alert("입력되지 않은 항목이 있습니다.");
-  } else if (
-    $("#signup_input_password").val().trim() !==
-    $("#signup_input_re_password").val().trim()
-  ) {
-    alert("비밀번호가 일치하지 않습니다.");
-  } else if (!$("input:radio[name=radiobutton1]:checked").val()) {
-    alert("체크할 항목이 남았습니다.");
+  if (!check_email()) {
+  } else if (!email_request || !email_responese) {
+    $("#signup_input_email_num").focus();
+  } else if (!check_name()) {
+    $("#signup_input_name").focus();
+  } else if (!check_password()) {
+    $("#signup_input_password").focus();
+  } else if (!check_re_password()) {
+    $("#signup_input_re_password").focus();
+  } else if (!radio_bool) {
+    alert("사용자 유형을 선택해주세요");
   } else {
     var formData = {
       email: $("#signup_input_email").val().trim(),
