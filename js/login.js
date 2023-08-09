85004e70d5ccd0e4cfe6acd709a8a57638d4bf60
kimyeonchul <yeonchul0807@nate.com>
2023-08-09 15:13:39 +0900

    Feat: add ajax function without url

--- js/login.js
+++ js/login.js
@@ -1,37 +1,36 @@
 $(".login_login_btn").click(function () {
   var idInput = $("#login_input_id");
   var pwdInput = $("#login_input_password");
-
   if (!idInput.val().trim()) {
     idInput.focus();
   } else if (!pwdInput.val().trim()) {
     pwdInput.focus();
   } else {
-    var formData = {
-      id: $("#login_input_id").val().trim(),
+    var postData = {
+      email: $("#login_input_id").val().trim(),
       password: $("#login_input_password").val().trim(),
     };
-    console.log(formData);
-    // $.ajax({
-    //   url: url,
-    //   type: "POST",
-    //   dataType: "json",
-    //   data: formData,
-    //   success: function (data) {
-    //     console.log(data);
-    //   },
-    //   error: function (request, status, error) {
-    //     console.log(
-    //       "code:" +
-    //         request.status +
-    //         "\n" +
-    //         "message:" +
-    //         request.responseText +
-    //         "\n" +
-    //         "error:" +
-    //         error
-    //     );
-    //   },
-    // });
+    // console.log(postData);
+    $.ajax({
+      url: "http://ec2-52-79-233-240.ap-northeast-2.compute.amazonaws.com/auth/token",
+      type: "POST",
+      dataType: "json",
+      data: JSON.stringify(postData),
+      contentType: "application/json",
+      success: function (data) {
+        console.log("sueccess: " + data);
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        if (jqXHR.status === 400) {
+          console.error("Bad Request:", jqXHR.responseText);
+          alert("형식이 일치하지 않습니다.");
+        } else if (jqXHR.status === 401) {
+          console.error("Unauthorized:", jqXHR.responseText);
+          alert("인증에 실패했습니다.");
+        } else {
+          console.error("Error:", jqXHR.status, errorThrown);
+        }
+      },
+    });
   }
 });
