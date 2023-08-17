7e632d71f223962c4f6ece01b427bc812f348f01
doctorwho14 <doctorwho14@naver.com>
2023-08-17 22:11:08 +0900

    Feat: 기업 프로필 뱃지 부분 연결 추가 (민기홍)

--- js/profile_ent.js
+++ js/profile_ent.js
@@ -21,6 +21,7 @@ $(".close_btn").click(function () {
 let badgeList = []; // 뱃지 배열을 badgeList에 저장
 var badgeListLength = badgeList.length; // 뱃지 개수
 let badgeImage = []; // 뱃지 이미지(썸네일) 리스트(bageList로 불러 올 수 있으면 굳이 필요 없을 수도있음)
+let badgeId = [];   // 뱃지의 id 값 받아서 넣을 배열
 var badgeDescription = []; // 뱃지 설명이 들어있는 배열
 
 /* ajax 부분 */
@@ -54,13 +55,18 @@ $.ajax({
   },
   success: function (data) {
     console.log("success:", JSON.stringify(data));
-
-    var Followers = data.follow.follower; // "api 사용자 프로필 조회" 에서 받아오게
-    var Following = data.follow.following; // "api 사용자 프로필 조회" 에서 받아오게
+    
+    
+    var Followers = data.follower; // "api 사용자 프로필 조회" 에서 받아오게
+    var Following = data.following; // "api 사용자 프로필 조회" 에서 받아오게
+    console.log("뱃지 테스트");
+    console.log(data.badge.items);
+    console.log(data.badge);
     myFollowers(Followers); // 팔로워 숫자 부분 html로 보내기
     myFollowing(Following); // 팔로우 숫자 부분 html로 보내기
+    
 
-    /* 
+  /* 
     뱃지 받아오는 부분
   */
   
@@ -68,8 +74,10 @@ $.ajax({
   $.each(data.badge.items, function (item) {
     badgeList.push(item); // 각각의 뱃지 썸네일을 빈 badgeImage 배열에 하나씩 푸쉬
   })
+  /*
   console.log("뱃지 배열 출력:");
   console.log(badgeList);
+  */
 
     // ajax 밖에 있는 빈 배열 badgeList에 뱃지의 data 넣기
     $.each(data.badge.items, function (item) {
@@ -88,35 +96,25 @@ $.ajax({
       badgeDescription.push(item.description);
     });
 
+    // 뱃지 추가하는 부분
+    showBadge(badgeListLength, badgeId);
+    $("#add_badge").click(function () {
+      window.location.href = "./making_badge.html";
+    })
+    
     //----------------------------------------------------
   },
   error: function (jqXHR, textStatus, errorThrown) {
     if (jqXHR.status === 404) {
-      console.error("404 Not Found", jqXHR.responseText);
+      //console.error("404 Not Found", jqXHR.responseText);
       alert("조회 하려는 유저가 존재하지 않을 때");
     }
   },
 });
 
 // get 부분(피드, 피드썸네일) (-> 피드를 눌렀을 때, 해당 피드로 이동)
-$.ajax({
-  url: `http://ec2-52-79-233-240.ap-northeast-2.compute.amazonaws.com/user/${userId}`,
-  type: "GET",
-  dataType: "json",
-  contentType: "application/json",
-  headers: {
-    Authorization: `Bearer ${jwtToken}`,
-  },
-  success: function (data) {
-    console.log("success:", JSON.stringify(data));
-  },
-  error: function (jqXHR, textStatus, errorThrown) {
-    if (jqXHR.status === 404) {
-      console.error("404 Not Found", jqXHR.responseText);
-      alert("조회 하려는 유저가 존재하지 않을 때");
-    }
-  },
-});
+
+
 
 let FeedList = [];
 let imageList = []; // 피드 값 중에서 image_urls 부분에서 첫 이미지들(image_urls[0])만 따로 받아오기(프로필 페이지 썸네일용)
@@ -141,17 +139,15 @@ function myFollowing(num) {
 
 FeedPosts(FeedListLength); // posts 숫자 부분
 
-function showBadge(num) {
+function showBadge(num, badgeIdList) {
   let template = ``;
 
   if (num != 0) {
     for (let i = 0; i < num; i++) {
       template += `
-      <div class="badge" id="badge${
-        i + 1
-      }"><img class="open_modal" onclick="modal_On(${0})" src="${
-        badgeList[i]
-      }"></div>
+      <div class="badge" id="badge${i + 1}" value="${badgeIdList[i]}">
+        <img class="open_modal" onclick="modal_On(${0})" src="${badgeImage[i]}">
+      </div>
       `;
     }
   }
@@ -161,16 +157,16 @@ function showBadge(num) {
   $(".badge_inner").append(template);
 }
 
+
 /*
-뱃지 눌렀을 때, 뱃지 모달창의 내용 변경 부분
+  뱃지 클릭 시 설명 페이지(badge_description.html)로 넘어가며 뱃지의 id를 파라미터로 준다.
 */
 // 이 부분은 badgeImage 배열 변수와, badgeList와 같이 있어야 됨.
-function modal_On(num) {
-  // showBadge(num) 함수가 실행되어야 실행되는 함수
-  document.querySelector(".modal_image").src = badgeImage[num]; // badgeList를 통해서 받아올 수 있으면 그걸로 간다.(하지만.. 지금처럼 이미지들만 따로 배열에 넣은게 편할 수도 있다...)
-  document.getElementsByClassName("modal_document")[0].innerText =
-    badgeDescription[num]; // 스택에서 보면 classname으로 받아오면 배열로 받아옴. 그래서 첫번째 요소로 받아와야 원하는대로 된다.
-  $(".modal_overlay").show(); // 모달 보여주는 코드
+
+function badgeClicked(num) {
+  // 뱃지 클릭 시 뱃지 설명 페이지(bagde_description.html로 넘어가)
+  var badge = document.getElementById(`badge${num}`);
+  window.location.href = `./badge_description.html?badgeId=${badge.attr("value")}`;
 }
 
 //---------------------------------------------------------------
@@ -200,7 +196,6 @@ function GoToFeed(num) {
 }
 
 showFeed(FeedList); // 피드의 개수에 따라 보여줄 화면에 대한 함수
-showBadge();
 
 /* 본인이 본인의 계정으로 들어온 것인지 확인하는 if문 (플러스 버튼의 유무를 위함) */
 // 세션에 본인꺼 currentAccountType(personal/business), currentUserId가 들어있다.
